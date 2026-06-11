2efd78e299d5c5966c1c41130f8567c8009d637e
Emir Cem Gezer <emircem.gezer@gmail.com>
2026-06-11 09:24:49 +0200

    feat: getting ready for automated publishing

--- src/mcsimpy/__init__.py
+++ src/mcsimpy/__init__.py
@@ -1,1 +1,8 @@
-__version__ = "0.0.1"
+"""Top-level package for mcsimpy."""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("mcsimpy")
+except PackageNotFoundError:
+    __version__ = "0+unknown"
--- src/mcsimpy/simulator/__init__.py
+++ src/mcsimpy/simulator/__init__.py
@@ -17,6 +17,5 @@ The following modules are found in the `mcsimpy.simulator`.
 """
 
 # Import all the models s.t we can access them from e.g.
-# from mcsimpy.simulator import CSAD_DP_6DOF
-# from mcsimpy.simulator.csad import CSAD_DP_6DOF, CSADMan3DOF, CSAD_DP_Seakeeping
-# from mcsimpy.simulator.gunnerus import GunnerusManeuvering3DoF, RVG_DP_6DOF
+from mcsimpy.simulator.csad import CSAD_DP_6DOF, CSADMan3DOF, CSAD_DP_Seakeeping
+from mcsimpy.simulator.gunnerus import GunnerusManeuvering3DoF, RVG_DP_6DOF
